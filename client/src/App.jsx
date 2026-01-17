d816aae7b9205f65d364916f8d61b2dd65cbe7c5
Richard Lefevre <rlefevre.6505@gmail.com>
2026-01-17 17:34:47 +0000

    adjusted styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <>
       <Header />
 
-      <Routes>
+      <Routes className="routes">
         <Route path="/" element={<Home />} />
         <Route path="/event" element={<Event />} />
         <Route path="/form" element={<Form />} />
